20f36356701b70a2744854100a1f1a7bb31fae22
Aman Rajput <95267159+Amanrajput1206@users.noreply.github.com>
2024-09-25 00:33:32 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+function App() {
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+  });
+
+  function handleChange(event) {
+    /* const fname = event.target.name; 
+       const value = event.target.value;
+       Alternatively You can use Destructuring as follows */
+    const { value, name } = event.target;
+    //The prevValue is a predefined value that gives the previous state of the useState variable i.e
+    // the current value before the set function takes effect
+    setFullName((prevValue) => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+        };
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+        };
+      }
+    });
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
+      <form>
+        <input
+          name="fName"
+          onChange={handleChange}
+          placeholder="First Name"
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          onChange={handleChange}
+          placeholder="Last Name"
+          value={fullName.lName}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
